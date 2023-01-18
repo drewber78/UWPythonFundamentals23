378a73a3a8b1c16b03a1b10a4d256243a47ee4ba
Drew Cochran <drew.j.cochran@outlook.com>
2023-01-17 16:16:23 -0800

    Changed to a custom function with a call to the function at the end to run the program

---
+++ Module01 - Python Basics/Module01 - Python Basics/Assignment01.py
@@ -0,0 +1,26 @@
+# Name: Drew cochran
+# Date: 12JAN2023
+# Description: Program to request a user to enter their name, with separate inputs for the first and last name,
+#               then print out the conjoined first and last name, pausing the program after and prompting the user
+#               to press "Enter" to end the program.
+
+
+# Custom function for this assignment
+def Enter_And_Print_Name():
+
+    # Print statement requesting user input
+    print("Please Enter Your First and Last Name...")
+
+    # Establishing user input variables for first and last name; prompt users to enter first and last name
+    first_name = input("First Name: ")
+    last_name = input("Last Name: ")
+
+    # Print out user inputted first and last name
+    print("You entered:  ", first_name, last_name)
+
+    # Ending input from user to close program
+    input("(Press Enter to End Program)")
+# End Enter_And_Print_Name function
+
+# Call the function
+Enter_And_Print_Name()
